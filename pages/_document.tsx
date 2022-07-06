7cfa32a66ced2e899c539eae3561f0942298123e
Ritik <ritik123453@gmail.com>
2022-07-06 21:57:35 +0530

    Fixing TypeScript errors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,14 @@
-import  Html from 'next/document'
-import  Head from 'next/document'
-import Main from 'next/document'
-import  NextScript  from 'next/document'
+import { Html } from 'next/document'
+import  {Head} from 'next/document'
+import {Main} from 'next/document'
 import Script from 'next/script'
+import { NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4947463072730532"
-     crossorigin="anonymous"></Script>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4947463072730532"></Script>
       </Head>
       <body>
         <Main />
